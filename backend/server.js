f0d0bda2ba38634317fd2ca8f8a3e52fbaa7d452
Pratyush Kukreja <pratyushkukreja@pratyushs-air.lan>
2024-11-30 22:04:30 -0800

    Update port configuration

--- backend/server.js
+++ backend/server.js
@@ -253,7 +253,7 @@ app.get('/scan', (req, res) => {
     res.sendFile(__dirname + '/card-scanner.html');
 });
 
-const PORT = 3000;
+cconst PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
